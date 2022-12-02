46d5997a086a1904992c644eeb57664e0793d213
mohamadfakir <113498626+mohamadfakir@users.noreply.github.com>
2022-12-02 14:51:31 +0530

    Add distance problem

--- src/day2hw/Distance.java
+++ src/day2hw/Distance.java
@@ -1,7 +1,9 @@
 package day2hw;
 
 public class Distance {
+    
     public static void main(String[]args){
+        
         int x = 5;
         int y = 14;
         double distance =Math.sqrt(x*x+y*y);
